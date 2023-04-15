80e780282f9dcb9a036a2e89ce75cb24600f3554
Stefan Williams <swilli73@icloud.com>
2023-04-15 17:17:11 -0400

    Adding deliverable 2 again

--- charts.js
+++ charts.js
@@ -112,7 +112,7 @@ function buildCharts(sample) {
       marker: {
         size: sample_values,
         color: otu_ids,
-        colorscale: "Pinkyl"
+        colorscale: "Picnic"
       }
 
     }];
